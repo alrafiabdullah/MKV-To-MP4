04c43c3113cec45936d23be81c922018870818a4
abdullah-al-rafi <abdullah34alrafi@gmail.com>
2021-04-05 00:29:16 +0600

    video file extentsion exception handled

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ mkv_list = os.listdir("assets")
 if not os.path.exists("result"):
     os.mkdir("result")
 
+
 for mkv in mkv_list:
     name, ext = os.path.splitext(mkv)
+    if ext != ".mkv":
+        print("Please add MKV files only!")
+        break
     output_name = name + ".mp4"
     os.system(
         f"cmd /c ffmpeg -i assets/{mkv} -codec copy result/{output_name}")
-
-print("Done")
